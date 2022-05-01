8e13c47c86de476f386d59c7e4d34f5bdbca8455
FightM9 <fightm9@gmail.com>
2022-05-01 14:58:50 +0300

    chore: insert `CommentSort` component

--- src/pages/Setting.tsx
+++ src/pages/Setting.tsx
@@ -1,26 +1,11 @@
-import { useAppDispatch, useAppSelector } from '../hooks/redux';
+import CommentSort from '../modules/CommentSort';
 import { Card } from '../style/Card';
-import { Form } from '../style/Form';
 
 const Setting = () => {
-  const dispath = useAppDispatch;
-  const { sort } = useAppSelector((state) => state.commentReducer);
-
-  // TODO: Fix select
-
   return (
     <main>
       <Card>
-        <Form>
-          <legend>Sort order</legend>
-          <select
-          // value={sort === 'ASC' ? 0 : 1}
-          // onChange={(e => dispath(setSort(evt.target.value)))}
-          >
-            <option value='ASC'>Ascending</option>
-            <option value='DESC'>Descending</option>
-          </select>
-        </Form>
+        <CommentSort />
       </Card>
     </main>
   );
