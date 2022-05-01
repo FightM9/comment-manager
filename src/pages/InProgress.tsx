be94b354651370c922f0febe6efae64b5b181da2
FightM9 <fightm9@gmail.com>
2022-05-01 14:55:18 +0300

    chore: add `Card` wraper

--- src/pages/InProgress.tsx
+++ src/pages/InProgress.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Card } from '../style/Card';
 
 const InProgress = () => {
   return (
     <main>
+      <Card>
       <h1>Warning.</h1>
       <p>This is a page in progress</p>
+      </Card>
     </main>
   );
 };
